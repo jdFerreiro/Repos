fa7536a680e257f3afa573d18a69bd1f20e6c862
David Ferreiro <fb.jdavid@gmail.com>
2019-10-21 20:45:16 -0400

    Add files via upload

---
+++ MyZadERP/MyZadERP/MyZadERP.iOS/Sevices/AppVersionAndBuild.cs
@@ -0,0 +1,18 @@
+﻿using Foundation;
+using MyZadERP.Interfaces;
+using MyZadERP.iOS.Sevices;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppVersionAndBuild))]
+namespace MyZadERP.iOS.Sevices
+{
+    public class AppVersionAndBuild : IAppVersionAndBuild
+    {
+
+        public string GetVersion()
+        {
+            string version = $"Version: {NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleShortVersionString").ToString()}.{NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleVersion").ToString()}";
+            return version;
+        }
+    }
+}
